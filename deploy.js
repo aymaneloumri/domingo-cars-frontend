975482e2773b4432a8e6280d8eb0d264cfebd2e0
aymaneloumri <ayman.eloumri@gmail.com>
2026-06-05 02:08:01 +0200

    feat: add FTP deploy script for Hostinger

---
+++ deploy.js
@@ -0,0 +1,19 @@
+const FtpDeploy = require('ftp-deploy')
+const ftpDeploy = new FtpDeploy()
+
+const config = {
+  user: 'u163327563.domingocars.ma',
+  password: process.env.FTP_PASSWORD,
+  host: '92.113.28.27',
+  port: 21,
+  localRoot: __dirname + '/dist',
+  remoteRoot: '/public_html/',
+  include: ['*', '**/*'],
+  deleteRemote: true,
+  forcePasv: true
+}
+
+ftpDeploy
+  .deploy(config)
+  .then(res => console.log('✅ Deploy finished:', res))
+  .catch(err => console.error('❌ Deploy error:', err))
